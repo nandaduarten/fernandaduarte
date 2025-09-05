dc6793636646ad447379fc4ca0997fb299007c44
nandaduarten <nandaduarten@gmail.com>
2025-09-05 00:28:58 -0300

    atualização 01

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -1,3 +1,13 @@
-function mudafoto(foto) {
-    document.getElementById("logo").src = foto;
-}
+// JavaScript Document
+  const links = document.querySelectorAll("aside nav a");
+  const sections = document.querySelectorAll("main section");
+
+  links.forEach(link => {
+    link.addEventListener("click", e => {
+      e.preventDefault();
+      const id = link.getAttribute("href").replace(".html","");
+
+      sections.forEach(sec => sec.style.display = "none"); // esconde tudo
+      document.getElementById(id).style.display = "block"; // mostra a seção clicada
+    });
+  });
